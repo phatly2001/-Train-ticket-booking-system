95efe33179e6bc42eefa0d1ecb859387f3df5f0b
Khoa <khoadh28@gmail.com>
2021-06-26 10:24:48 +0700

    Fisrt commit

---
+++ src/test/java/sopvn/Trainticketbookingsystem/TrainTicketBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package sopvn.Trainticketbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainTicketBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
